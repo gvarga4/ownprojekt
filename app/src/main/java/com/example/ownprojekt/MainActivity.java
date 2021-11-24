8c5b36efab3cf90693c7d2441c7bb0636469149d
Gabriella Varga <g.varga@AD.SINGEN-HGS.LOGODIDACT.NET>
2021-11-24 11:48:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ownprojekt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ownprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
